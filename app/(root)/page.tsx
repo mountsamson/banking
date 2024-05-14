87016f098a161ce02816de7aee39ccdcb6c5be2e
mountsamson <carl.francis.samson@gmail.com>
2024-05-14 19:41:13 +1000

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
-const page = () => {
-  return <div>home</div>;
+const Home = () => {
+  const loggedIn = {
+    firstName: "Carl",
+    lastName: "Samson",
+    email: "samson@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext=" Access and manage your acount and transactions smoothly."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.32}
+          />
+        </header>
+        RECENT TRACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1223.5 }, { currentBalance: 199.23 }]}
+      />
+    </section>
+  );
 };
 
-export default page;
+export default Home;
